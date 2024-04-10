d3fd1febbc0232216dff0ac0f9ba8916ec2a1cdc
Alexey Bryncev <alxbrc0@gmail.com>
2024-04-10 17:30:01 +0300

    Multiprocessing upgrade

--- bench.py
+++ bench.py
@@ -1,4 +1,4 @@
-from os import path, listdir
+from os import path, listdir, mkdir
 from tqdm import tqdm
 import numpy as np
 from glob import glob
@@ -68,55 +68,65 @@ def kldiv(s_map, gt):
 
 ######
 
-def calculate_metrics(video_name, temp_predictions_path, temp_gt_saliency_path, temp_gt_fixations_path, is_first, robust_metric_res):
+def calculate_frame_metrics(frame):
+    gt_fix = padding_fixation(read_sm(frame['gt_fixations_path']))
+    gt_120_sm = padding(read_sm(frame['gt_saliency_path']))
+    pred_sm = padding(read_sm(frame['predictions_path']))
+
+    if robust_metric_res is not None:
+        pred_sm = np.clip(apply_transforms(pred_sm, frame['robust_metric_res'][0], frame['robust_metric_res'][1]), 0, 1)
+
+    return {
+        'sim_score': similarity(pred_sm, gt_120_sm),
+        'nss_score': nss(pred_sm, gt_fix),
+        'cc_score': cc(pred_sm, gt_120_sm),
+        'auc_judd_score': auc_judd(pred_sm, gt_fix),
+        'kldiv_score': kldiv(pred_sm, gt_120_sm)
+    }
+
+
+def calculate_metrics(video_name, temp_predictions_path, temp_gt_saliency_path, temp_gt_fixations_path, robust_metric_res, num_workers):
     predictions_path = glob(temp_predictions_path)[0]
     gt_saliency_path = glob(temp_gt_saliency_path)[0]
     gt_fixations_path = glob(temp_gt_fixations_path)[0]
 
-    sim_score = []
-    nss_score = []
-    cc_score = []
-    auc_judd_score = []
-    kldiv_score = []
-
+    scores = []
     assert_func = lambda path: set([int(x.split('.')[0]) for x in listdir(path)])
     assert assert_func(gt_fixations_path) == assert_func(gt_saliency_path) == assert_func(predictions_path)
     
-    frames = zip(sorted(listdir(gt_fixations_path)), sorted(listdir(gt_saliency_path)), sorted(listdir(predictions_path)))
-    frames_with_tqdm = tqdm(frames, total=len(listdir(gt_fixations_path))) if is_first else frames
-    for frame in frames_with_tqdm:
-        gt_fix = padding_fixation(read_sm(path.join(gt_fixations_path, frame[0])))
-        gt_120_sm = padding(read_sm(path.join(gt_saliency_path, frame[1])))
-        pred_sm = padding(read_sm(path.join(predictions_path, frame[2])))
-
-        if robust_metric_res is not None:
-            pred_sm = np.clip(apply_transforms(pred_sm, robust_metric_res[0], robust_metric_res[1]), 0, 1)
-
-        sim_score += [similarity(pred_sm, gt_120_sm)]
-        nss_score += [nss(pred_sm, gt_fix)]
-        cc_score += [cc(pred_sm, gt_120_sm)]
-        auc_judd_score += [auc_judd(pred_sm, gt_fix)]
-        kldiv_score += [kldiv(pred_sm, gt_120_sm)]
+    frames = [
+        {
+            'gt_fixations_path': gt_fix, 
+            'gt_saliency_path': gt_sal, 
+            'predictions_path': pred,
+            'robust_metric_res': robust_metric_res
+        } for gt_fix, gt_sal, pred in zip(
+            [path.join(gt_fixations_path, x) for x in sorted(listdir(gt_fixations_path))], 
+            [path.join(gt_saliency_path, x) for x in sorted(listdir(gt_saliency_path))], 
+            [path.join(predictions_path, x) for x in sorted(listdir(predictions_path))]
+    )]
+    with Pool(num_workers) as pool:
+        scores = pool.map(calculate_frame_metrics, frames)
+    
+    conv_scores = {metric: [x[metric] for x in scores] for metric in scores[0].keys()}
 
     return {'video_name' : video_name,
-            'cc' : np.mean(cc_score),
-            'sim' : np.mean(sim_score),
-            'nss' : np.mean(nss_score),
-            'auc_judd' : np.mean(auc_judd_score),
-            'kldiv' : np.mean(kldiv_score)}
+            'cc' : np.mean(conv_scores['cc_score']),
+            'sim' : np.mean(conv_scores['sim_score']),
+            'nss' : np.mean(conv_scores['nss_score']),
+            'auc_judd' : np.mean(conv_scores['auc_judd_score']),
+            'kldiv' : np.mean(conv_scores['kldiv_score'])}
 
 
-def poolfunc(batch):
+def calculate_all_videos(video_names, robust_metric_res, num_workers):
     detail_result = []
-    for video_name in batch[0]:
+    for video_name in tqdm(video_names):
         full_video_name = f'{video_name}*'
         model_output = path.join(FROM_MODEL, full_video_name)
         gt_gaussians = path.join(GT, full_video_name, 'gaussians')
         gt_fixations = path.join(GT, full_video_name, 'fixations')
 
-        if batch[1]:
-            print(video_name)
-        detail_result += [calculate_metrics(video_name, model_output, gt_gaussians, gt_fixations, batch[1], batch[2])]
+        detail_result += [calculate_metrics(video_name, model_output, gt_gaussians, gt_fixations, robust_metric_res, num_workers)]
     
     return detail_result
 
@@ -124,18 +134,19 @@ def poolfunc(batch):
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description='Compares multiple models by a variety of metrics using multithreaded data processing')
-    parser.add_argument('--models_root', dest='models_root', help='path to directory with models predictions', default='savam_from_')
-    parser.add_argument('--gt_root', dest='GT', help='path to directory with Ground Truth saliency maps and fixations', default='savam_GT')
-    parser.add_argument('--dont_use_domain_adaptation', dest='dont_use_domain_adaptation', action='store_true', help='specifies not to use domain adaptation')
-    parser.add_argument('--num_workers', dest='num_workers', type=int, help='number of used threads', default=1)
+    parser.add_argument('--models-root', help='path to directory with models predictions', default='savam_from_')
+    parser.add_argument('--gt-root', dest='GT', help='path to directory with Ground Truth saliency maps and fixations', default='savam_GT')
+    parser.add_argument('--dont-use-domain-adaptation', action='store_true', help='specifies not to use domain adaptation')
+    parser.add_argument('--num-workers', type=int, help='number of used threads', default=1)
+    parser.add_argument('--skip-incomplete-results-test', action='store_true', help='allowed the results of the tested model do not contain all the videos of the dataset')
     args = parser.parse_args()
     models_root = args.models_root
     GT = args.GT
     use_robust_metric = not args.dont_use_domain_adaptation
     num_workers = args.num_workers
+    skip_inc_res = args.skip_incomplete_results_test
     
     print(num_workers, 'worker(s)')
-    video_names = sorted(listdir(GT))
 
     models_num = len(listdir(models_root))
     for model_num, model_root in enumerate(sorted(listdir(models_root))):
@@ -147,26 +158,28 @@ if __name__ == '__main__':
             def read_folder(folder_dir):
                 return sorted([path.join(folder_dir, p) for p in listdir(folder_dir)])
 
-            sm_maps = list(map(lambda x: read_folder(path.join(FROM_MODEL, x)), sorted(listdir(FROM_MODEL))))
-            gt_maps = list(map(lambda x: read_folder(path.join(GT, x, 'gaussians')), sorted(listdir(GT))))
+            sm_listdir = listdir(FROM_MODEL)
+            GT_listdir = listdir(GT)
+            if len(sm_listdir) < len(GT_listdir):
+                msg = f'There are results for only a few videos ({len(sm_listdir)}/{len(GT_listdir)})!'
+                if skip_inc_res:
+                    print(f'Warning: {msg}')
+                else:
+                    raise ValueError(msg)
+            video_names = sorted(sm_listdir)
+
+            GT_listdir_filtered = [x for x in GT_listdir if x in sm_listdir]
+            sm_maps = list(map(lambda x: read_folder(path.join(FROM_MODEL, x)), sorted(sm_listdir)))
+            gt_maps = list(map(lambda x: read_folder(path.join(GT, x, 'gaussians')), sorted(GT_listdir_filtered)))
             cp_img = padding(read_sm('gt_cp.png'))
             robust_metric_res = cp_img, ss_robust_metric2(sm_maps, gt_maps, cp_img, nbins=256, num_workers=num_workers)
 
-        batch_size = len(video_names) // num_workers
-        borders = [[batch_size * i, batch_size * (i + 1)] for i in range(num_workers)]
-        borders[-1][1] = len(video_names)
-        splitted_infos = [[video_names[l:r], False, robust_metric_res] for [l, r] in borders]
-        splitted_infos[-1][1] = True
-
-        with Pool(num_workers) as pool:
-            detail_result = pool.map(poolfunc, splitted_infos)
-
-        detail_result = chain(*detail_result)
+        detail_result = calculate_all_videos(video_names, robust_metric_res, num_workers)
 
         result_name = 'Robust_Result' if use_robust_metric else 'Result'
-
-        model_name = '_'.join(model_root.split('_')[2:])
-        json_root = path.join(result_name, model_name)
+        if not path.exists(result_name):
+            mkdir(result_name)
+        json_root = path.join(result_name, model_root)
         detail_result = sorted(detail_result, key=lambda res: res['video_name'])
         with open(f'{json_root}.json', mode='w') as output:
             output.write(json.dumps(detail_result))
@@ -176,8 +189,8 @@ if __name__ == '__main__':
             for j in detail_result:
                 result[i].append(j[i])
 
-        model_res = {'model': model_name}
-        [model_res.update({key: [np.mean(result[key])]}) for key in result.keys()]
+        model_res = {'model': model_root}
+        [model_res.update({key: np.mean(result[key])}) for key in result.keys()]
         header = not path.exists(f'{result_name}.csv')
         pd.DataFrame.from_dict(model_res, orient='columns').to_csv(f'{result_name}.csv', mode='a', header=header, index=False)
         print(model_res)
